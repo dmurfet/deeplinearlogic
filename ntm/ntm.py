4be64ab068381873ddb2433c595df9a9f2e1e33d
Daniel Murfet <daniel.murfet@gmail.com>
2017-03-28 16:56:54 +1100

    Testing new Pattern NTM

--- ntm/ntm.py
+++ ntm/ntm.py
@@ -266,15 +266,14 @@ class PatternNTM(RNNCell):
     The main Pattern NTM code.
     """
     def __init__(self, num_units, input_size, controller_state_size,
-                memory_address_size,memory_content_size, 
-                powers1, powers2, powers21):
+                memory_address_sizes,memory_content_sizes, 
+                powers, powers21):
         self._num_units = num_units
         self._input_size = input_size
         self._controller_state_size = controller_state_size
-        self._memory_address_size = memory_address_size
-        self._memory_content_size = memory_content_size
-        self._powers1 = powers1
-        self._powers2 = powers2
+        self._memory_address_sizes = memory_address_sizes
+        self._memory_content_sizes = memory_content_sizes
+        self._powers = powers
         self._powers21 = powers21
         
     @property
@@ -294,194 +293,173 @@ class PatternNTM(RNNCell):
         with tf.variable_scope(scope,reuse=reuse): 
             # input has shape [batch_size, input_size]
             # state has shape [batch_size, state_size]
-            
+
+            css = self._controller_state_size
+            mas = self._memory_address_sizes
+            mcs = self._memory_content_sizes
+            num_rings = len(mas)
+                        
             # We divide up a state vector as follows:
             #
-            # h = (h0,r1,w1,r2,w2,M1,M2)
+            # h = (h0,r1,w1,...,rN,wN,M1,M2,...,MN)
             #
             # where
             #
-            #   - h0 is the controller internal state (size controller_state_size)
-            #   - ri is the ith read address weights (size memory_address_size)
-            #   - wi is the ith write address weights (size memory_address_size)
-            #   - Mi is the ith memory state (size memory_address_size*memory_content_size)
+            #   - h0 is the controller internal state (size css)
+            #   - ri is the ith read address weights (size mas[i])
+            #   - wi is the ith write address weights (size mcs[i])
+            #   - Mi is the ith memory state (size mas[i]*mcs[i])
             #
-            # Viewed as a matrix of shape [mas,mcs] the rows of M index memory locations.
+            # Viewed as a matrix of shape [mas[i],mcs[i]] the rows of M[i] index memory locations.
             #
             # NOTE: these vectors are all batched, so in the following h0 has shape
             # [batch_size, controller_state_size], for example.
             
-            css = self._controller_state_size
-            mas = self._memory_address_size
-            mcs = self._memory_content_size
             
-            powers1 = self._powers1 # the powers of the rotation matrix the controller uses on ring 1
-            powers2 = self._powers2 # the powers of the rotation matrix the controller uses on ring 2
+            powers = self._powers # the powers of the rotation matrix the controller uses on rings
             powers21 = self._powers21 # the powers applied by ring 2 to ring 1
 
-            mcs2 = len(powers21)
+            h0, r1, w1, r2, w2, r3, w3, M1, M2, M3 = tf.split(state, [css, mas[0], mas[0], 
+                                                        mas[1], mas[1], mas[2], mas[2],
+                                                        mas[0] * mcs[0], mas[1] * mcs[1],
+                                                        mas[2] * mcs[2]], 1)
             
-            h0, r1, w1, r2, w2, interp, M1, M2 = tf.split(state, [css, mas, mas, mas, mas, 2, mas * mcs, mas * len(powers21)], 1)
+            r = [r1,r2,r3]
+            w = [w1,w2,w3]
+            M = [M1,M2,M3]
             
             init = init_ops.constant_initializer(0.0)
             
-            # Note that M2 is [mas, mcs2]
-            
             # Sharpening factor gamma, one for read and one for write, for each ring
-            W_gamma_read1 = tf.get_variable("W_gamma_read1", [css,1])
-            B_gamma_read1 = tf.get_variable("B_gamma_read1", [], initializer=init)
-            gamma_read1 = 1.0 + tf.nn.relu(tf.matmul(h0,W_gamma_read1) + B_gamma_read1) # shape [batch_size,1]
+            gamma_read_tensors = []
+            gamma_write_tensors = []
             
-            W_gamma_write1 = tf.get_variable("W_gamma_write1", [css,1])
-            B_gamma_write1 = tf.get_variable("B_gamma_write1", [], initializer=init)
-            gamma_write1 = 1.0 + tf.nn.relu(tf.matmul(h0,W_gamma_write1) + B_gamma_write1) # shape [batch_size,1]
-
-            W_gamma_read2 = tf.get_variable("W_gamma_read2", [css,1])
-            B_gamma_read2 = tf.get_variable("B_gamma_read2", [], initializer=init)
-            gamma_read2 = 1.0 + tf.nn.relu(tf.matmul(h0,W_gamma_read2) + B_gamma_read2) # shape [batch_size,1]
-            
-            W_gamma_write2 = tf.get_variable("W_gamma_write2", [css,1])
-            B_gamma_write2 = tf.get_variable("B_gamma_write2", [], initializer=init)
-            gamma_write2 = 1.0 + tf.nn.relu(tf.matmul(h0,W_gamma_write2) + B_gamma_write2) # shape [batch_size,1]
-
-            W_gamma_interp = tf.get_variable("W_gamma_interp", [css,1])
-            B_gamma_interp = tf.get_variable("B_gamma_interp", [], initializer=init)
-            gamma_interp = 1.0 + tf.nn.relu(tf.matmul(h0,W_gamma_interp) + B_gamma_interp) # shape [batch_size,1]
-
+            for i in range(num_rings):
+                W_gamma_read = tf.get_variable("W_gamma_read" + str(i + 1), [css,1])
+                B_gamma_read = tf.get_variable("B_gamma_read" + str(i + 1), [], initializer=init)
+                gamma_read = 1.0 + tf.nn.relu(tf.matmul(h0,W_gamma_read) + B_gamma_read) # shape [batch_size,1]
+                
+                gamma_read_tensors.append(gamma_read)
+                
+                W_gamma_write = tf.get_variable("W_gamma_write" + str(i + 1), [css,1])
+                B_gamma_write = tf.get_variable("B_gamma_write" + str(i + 1), [], initializer=init)
+                gamma_write = 1.0 + tf.nn.relu(tf.matmul(h0,W_gamma_write) + B_gamma_write) # shape [batch_size,1]
+                
+                gamma_write_tensors.append(gamma_write)
+                
             # Now generate the s, q, e, a vectors
-            W_s1 = tf.get_variable("W_s1", [css,len(powers1)])
-            B_s1 = tf.get_variable("B_s1", [len(powers1)], initializer=init)
-            s1 = tf.nn.softmax(tf.matmul(h0,W_s1) + B_s1) # shape [batch_size,len(powers1)]
-
-            W_s2 = tf.get_variable("W_s2", [css,len(powers2)])
-            B_s2 = tf.get_variable("B_s2", [len(powers2)], initializer=init)
-            s2 = tf.nn.softmax(tf.matmul(h0,W_s2) + B_s2) # shape [batch_size,len(powers2)]
-
-            W_q1 = tf.get_variable("W_q1", [css,len(powers1)])
-            B_q1 = tf.get_variable("B_q1", [len(powers1)], initializer=init)
-            q1 = tf.nn.softmax(tf.matmul(h0,W_q1) + B_q1) # shape [batch_size,len(powers1)]
-
-            W_q2 = tf.get_variable("W_q2", [css,len(powers2)])
-            B_q2 = tf.get_variable("B_q2", [len(powers2)], initializer=init)
-            q2 = tf.nn.softmax(tf.matmul(h0,W_q2) + B_q2) # shape [batch_size,len(powers2)]
-
-            W_e1 = tf.get_variable("W_e1", [css,mcs])
-            B_e1 = tf.get_variable("B_e1", [mcs], initializer=init)
-            e1 = tf.sigmoid(tf.matmul(h0,W_e1) + B_e1) # shape [batch_size,mcs]
-
-            W_e2 = tf.get_variable("W_e2", [css,mcs2])
-            B_e2 = tf.get_variable("B_e2", [mcs2], initializer=init)
-            e2 = tf.sigmoid(tf.matmul(h0,W_e2) + B_e2) # shape [batch_size,mcs2]
-
-            W_a1 = tf.get_variable("W_a1", [css,mcs])
-            B_a1 = tf.get_variable("B_a1", [mcs], initializer=init)
-            a1 = tf.nn.relu(tf.matmul(h0,W_a1) + B_a1) # shape [batch_size,mcs]
-
-            W_a2 = tf.get_variable("W_a2", [css,mcs2])
-            B_a2 = tf.get_variable("B_a2", [mcs2], initializer=init)
-            a2 = tf.nn.softmax(tf.matmul(h0,W_a2) + B_a2) # shape [batch_size,mcs2]
-            # DEBUG note this is a softmax
+            s_tensors = []
+            q_tensors = []
+            e_tensors = []
+            a_tensors = []
             
-            W_s_interp = tf.get_variable("W_s_interp", [css,2])
-            B_s_interp = tf.get_variable("B_s_interp", [2], initializer=init)
-            s_interp = tf.nn.softmax(tf.matmul(h0,W_s_interp) + B_s_interp) # shape [batch_size,2]
-
+            for i in range(num_rings):
+                W_s = tf.get_variable("W_s" + str(i+1), [css,len(powers[i])])
+                B_s = tf.get_variable("B_s" + str(i+1), [len(powers[i])], initializer=init)
+                s = tf.nn.softmax(tf.matmul(h0,W_s) + B_s) # shape [batch_size,len(powers[i])]
+                s_tensors.append(s)
+                
+                W_q = tf.get_variable("W_q" + str(i+1), [css,len(powers[i])])
+                B_q = tf.get_variable("B_q" + str(i+1), [len(powers[i])], initializer=init)
+                q = tf.nn.softmax(tf.matmul(h0,W_q) + B_q) # shape [batch_size,len(powers[i])]
+                q_tensors.append(q)
+                
+                W_e = tf.get_variable("W_e" + str(i+1), [css,mcs[i]])
+                B_e = tf.get_variable("B_e" + str(i+1), [mcs[i]], initializer=init)
+                e = tf.sigmoid(tf.matmul(h0,W_e) + B_e) # shape [batch_size,mcs]
+                e_tensors.append(e)
+                
+                W_a = tf.get_variable("W_a" + str(i+1), [css,mcs[i]])
+                B_a = tf.get_variable("B_a" + str(i+1), [mcs[i]], initializer=init)
+                a = tf.nn.relu(tf.matmul(h0,W_a) + B_a) # shape [batch_size,mcs]
+                a_tensors.append(a)
+                
+            # DEBUG maybe add on ring 2 should be softmax?
+            
             # Add and forget on the memory
-            M1 = tf.reshape(M1, [-1, mas, mcs])
-            erase_term1 = tf.matmul( M1, tf.matrix_diag(e1) ) # shape [batch_size, mas, mcs]
-            add_term1 = tf.matmul( tf.reshape(w1,[-1,mas,1]), tf.reshape(a1,[-1,1,mcs]) ) # shape [batch_size, mas, mcs]
-            M1_new = M1 - erase_term1 + add_term1
-            M1_new = tf.reshape(M1_new, [-1, mas * mcs])
-
-            M2 = tf.reshape(M2, [-1, mas, mcs2])
-            erase_term2 = tf.matmul( M2, tf.matrix_diag(e2) ) # shape [batch_size, mas, mcs2]
-            add_term2 = tf.matmul( tf.reshape(w2,[-1,mas,1]), tf.reshape(a2,[-1,1,mcs2]) ) # shape [batch_size, mas, mcs2]
-            M2_new = M2 - erase_term2 + add_term2
-            M2_new = tf.reshape(M2_new, [-1, mas * mcs2])
+            M_news = []
+            
+            for i in range(num_rings):
+                M[i] = tf.reshape(M[i], [-1, mas[i], mcs[i]])
+                erase_term = tf.matmul( M[i], tf.matrix_diag(e_tensors[i]) ) # shape [batch_size, mas[i], mcs[i]]
+                add_term = tf.matmul( tf.reshape(w[i],[-1,mas[i],1]), tf.reshape(a_tensors[i],[-1,1,mcs[i]]) ) # shape [batch_size, mas[i], mcs[i]]
+                M_new = M[i] - erase_term + add_term
+                M_new = tf.reshape(M_new, [-1, mas[i] * mcs[i]])
+                M_news.append(M_new)
                         
             # Do the rotations of the read and write addresses
-            Rtensor_interp = rotation_tensor(2,[0,1])
-            Rtensor1 = rotation_tensor(mas,powers1)
-            Rtensor1_21 = rotation_tensor(mas,powers1 + powers21)
-            Rtensor2 = rotation_tensor(mas,powers2)
+            r_news = []
+            w_news = []
             
-            # The new thing in the pattern NTM is 
-            Mr2 = tf.matmul( M2, tf.reshape(r2,[-1,mas,1]), transpose_a=True )
-            Mr2 = tf.reshape( Mr2, [-1,mcs2] )           
-            
-            # Interpolate between Mr2 and q1
-            interp = tf.reshape( interp, [-1, 2, 1] )
-            q1_pluszeros = tf.concat([q1, tf.zeros_like(Mr2)], axis=1)
-            Mr2_pluszeros = tf.concat([tf.zeros_like(q1), Mr2], axis=1)
-            
-            q_interp = tf.matmul( tf.stack([q1_pluszeros, Mr2_pluszeros], axis=2), interp )
-            q_interp = tf.reshape( q_interp, [-1, len(powers1) + len(powers21)] )
-            
-            r1_new = tf.matmul( tf.reshape(r1, [-1,1,mas]),
-                                tf.tensordot( q_interp, Rtensor1_21, [[1], [0]] ) )
-            
-            # Rotate interpolation
-            interp_new = tf.matmul( tf.reshape(interp, [-1,1,2]),
-                                tf.tensordot( s_interp, Rtensor_interp, [[1], [0]] ) )
-            interp_new = tf.reshape( interp_new, [-1,2] )
- 
-            # yields a tensor of shape [batch_size, mas, mas]
-            # each row of which is \sum_i q_i R^i, and this batch
-            # of matrices is then applied to r to generate r_new
-            # NOTE: These are actually batch matmuls (tf.batch_matmul
-            # went away with v1.0, matmul now does it automatically on the
-            # first index)
-                                           
-            w1_new = tf.matmul( tf.reshape(w1, [-1,1,mas]),
-                                tf.tensordot( s1, Rtensor1, [[1], [0]] ) )
-                                
-            r2_new = tf.matmul( tf.reshape(r2, [-1,1,mas]),
-                                tf.tensordot( q2, Rtensor2, [[1], [0]] ) )
-            w2_new = tf.matmul( tf.reshape(w2, [-1,1,mas]),
-                                tf.tensordot( s2, Rtensor2, [[1], [0]] ) )
-                 
-            r1_new = tf.reshape( r1_new, [-1,mas] )
-            w1_new = tf.reshape( w1_new, [-1,mas] )
-            r2_new = tf.reshape( r2_new, [-1,mas] )
-            w2_new = tf.reshape( w2_new, [-1,mas] )            
-
-            # Perform sharpening
-            sharpening_tensor_r1 = tf.zeros_like(r1_new) + gamma_read1
-            sharp_r1 = tf.pow(r1_new + 1e-6, sharpening_tensor_r1)
-            denom_r1 = tf.reduce_sum(sharp_r1, axis=1, keep_dims=True)
-            r1_new = sharp_r1 / denom_r1
-            
-            sharpening_tensor_w1 = tf.zeros_like(w1_new) + gamma_write1
-            sharp_w1 = tf.pow(w1_new + 1e-6, sharpening_tensor_w1)
-            denom_w1 = tf.reduce_sum(sharp_w1, axis=1, keep_dims=True)
-            w1_new = sharp_w1 / denom_w1
+            for i in range(num_rings):
+                rot = rotation_tensor(mas[i],powers[i])
                 
-            sharpening_tensor_r2 = tf.zeros_like(r2_new) + gamma_read2
-            sharp_r2 = tf.pow(r2_new + 1e-6, sharpening_tensor_r2)
-            denom_r2 = tf.reduce_sum(sharp_r2, axis=1, keep_dims=True)
-            r2_new = sharp_r2 / denom_r2
+                w_new = tf.matmul( tf.reshape(w[i], [-1,1,mas[i]]),
+                                tf.tensordot( s_tensors[i], rot, [[1], [0]] ) )
+                r_new = tf.matmul( tf.reshape(r[i], [-1,1,mas[i]]),
+                                tf.tensordot( q_tensors[i], rot, [[1], [0]] ) )
+               
+                r_new = tf.reshape( r_new, [-1,mas[i]] )
+                w_new = tf.reshape( w_new, [-1,mas[i]] )
+                r_news.append(r_new)
+                w_news.append(w_new)
             
-            sharpening_tensor_w2 = tf.zeros_like(w2_new) + gamma_write2
-            sharp_w2 = tf.pow(w2_new + 1e-6, sharpening_tensor_w2)
-            denom_w2 = tf.reduce_sum(sharp_w2, axis=1, keep_dims=True)
-            w2_new = sharp_w2 / denom_w2
-
-            sharpening_tensor_interp = tf.zeros_like(interp_new) + gamma_interp
-            sharp_interp = tf.pow(interp_new + 1e-6, sharpening_tensor_interp)
-            denom_interp = tf.reduce_sum(sharp_interp, axis=1, keep_dims=True)
-            interp_new = sharp_interp / denom_interp
+            # Special Pattern NTM stuff. We use the current contents of the
+            # second memory ring to generate a distribution over rotations
+            # of the first memory ring, and then act with those rotations on
+            # the read address of the first memory ring. 
             
+            rot = rotation_tensor(mas[0], powers21)
+                
+            Mr2 = tf.matmul( M[1], tf.reshape(r[1],[-1,mas[1],1]), transpose_a=True )
+            Mr2 = tf.reshape( Mr2, [-1,mcs[1]] )           
+            
+            Mr3 = tf.matmul( M[2], tf.reshape(r[2],[-1,mas[2],1]), transpose_a=True ) 
+            Mr3 = tf.reshape( Mr3, [-1,1] )
+            
+            # ASSUME mcs[1] = len(powers21)
+            # We read the content of the third memory ring as a weight
+            Mr2_rot = tf.tensordot( Mr2, rot, [[1], [0]] ) # shape [batch_size, mas[0], mas[0]]
+            
+            ident_mat = tf.diag(tf.ones([1,mas[0]], tf.float32)) # shape [mas[0], mas[0]]
+            ident_mat = tf.reshape( ident_mat, [1, mas[0], mas[0]] )
+            Mr3 = tf.tensordot( Mr3, ident_mat, [[1], [0]] ) # shape [batch_size, mas[0], mas[0]]
+            
+            rot_matrix = tf.matmul( Mr2_rot, Mr3 ) # shape [batch_size, mas[0], mas[0]]
+            r_news[0] = tf.matmul( tf.reshape(r_news[0], [-1,1,mas[0]]), rot_matrix )
+            r_news[0] = tf.reshape( r_news[0], [-1,mas[0]] )
+            
+            # Perform sharpening
+            for i in range(num_rings):
+                r_new = r_news[i]
+                sharpening_tensor_r = tf.zeros_like(r_new) + gamma_read_tensors[i]
+                sharp_r = tf.pow(r_new + 1e-6, sharpening_tensor_r)
+                denom_r = tf.reduce_sum(sharp_r, axis=1, keep_dims=True)
+                r_new = sharp_r / denom_r
+                r_news[i] = r_new
+                
+                w_new = w_news[i]
+                sharpening_tensor_w = tf.zeros_like(w_new) + gamma_write_tensors[i]
+                sharp_w = tf.pow(w_new + 1e-6, sharpening_tensor_w)
+                denom_w = tf.reduce_sum(sharp_w, axis=1, keep_dims=True)
+                w_new = sharp_w / denom_w
+                w_news[i] = w_new
+                            
             # Now the usual RNN stuff
             H = tf.get_variable("H", [css,css])
             U = tf.get_variable("U", [self._input_size,css])
             B = tf.get_variable("B", [css], initializer=init)
         
-            V = tf.get_variable("V", [mcs,css]) # converts from memory to controller state
-            Mr1 = tf.matmul( M1, tf.reshape(r1,[-1,mas,1]), transpose_a=True )
-            Mr1 = tf.reshape( Mr1, [-1,mcs] )
+            # Special Pattern NTM stuff, read from the first memory ring
+            V = tf.get_variable("V", [mcs[0],css]) # converts from memory to controller state
+            Mr1 = tf.matmul( M[0], tf.reshape(r[0],[-1,mas[0],1]), transpose_a=True )
+            Mr1 = tf.reshape( Mr1, [-1,mcs[0]] )
             
             h0_new = tf.nn.tanh(tf.matmul(h0, H) + tf.matmul(Mr1,V) + tf.matmul(input,U) + B)
                     
-            state_new = tf.concat([h0_new, r1_new, w1_new, r2_new, w2_new, interp_new, M1_new, M2_new], 1)   
+            state_new = tf.concat([h0_new, r_news[0], w_news[0],
+                                        r_news[1], w_news[1],
+                                        r_news[2], w_news[2],
+                                        M_news[0], M_news[1], M_news[2]], 1)   
         return h0_new, state_new
         # the return is output, state
